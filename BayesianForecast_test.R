45109942fddf64d5dc0dd988b2f06a0cf7a78e18
nmset0 <nmset0@colostate.edu>
2025-04-05 17:04:57 -0600

    update

--- BayesianForecast_test.R
+++ BayesianForecast_test.R
@@ -2,40 +2,103 @@ rm(list = ls())
 
 library(tidyverse)
 library(readxl)
-library(rstan)
+library(lubridate)
 library(bayesforecast)
 
 FinalData <- read_excel("Data/FinalData.xlsx")
-FinalData <- FinalData |> dplyr::select(-1) |> dplyr::select(-Licenses) |> dplyr::select(-c(2,3,4,5,6))
+# FinalData <- FinalData |> dplyr::select(-1) |> dplyr::select(-Licenses, -Lead_STF)
+FinalData <- FinalData |> dplyr::select(-Licenses)
+FinalData$Month <- month(FinalData$Month)
+FinalData$Lead_STF_Real <- FinalData$Lead_STF_Real * (3.85/4.25)
+FinalData$EDUHS[1:12] <- FinalData$EDUHS[1:12] - 5
+FinalData$SG[1:12] <- FinalData$SG[1:12] + 5
+FinalData <- FinalData[-nrow(FinalData),]
+
+
+ldf <- log(FinalData)
+numrow <- nrow(ldf)
+ldf2023 <- ldf[1:(numrow- 12),]
+
+ts.object_2023 <- ts(ldf2023$Lead_STF_Real, start = c(2012, 1), frequency = 12)
+
+sf_2023 <- stan_sarima(ts = ts.object_2023, order = c(0,0,0), seasonal = c(1,1,1),
+                   prior_mu0 = student(mu = 0, sd = 1, df = nrow(ldf2023)-1), chains = 5)
+# Bayesian forecasting
+Stan_SalesTax_Forecast <- forecast(sf_2023, h = 12)
+Stan_SalesTax_Forecast_2024 <- as.data.frame(Stan_SalesTax_Forecast)
+Stan_SalesTax_Forecast_2024$`Point Forecast` <- exp(Stan_SalesTax_Forecast_2024$`Point Forecast`)
+
+Forecast2024 <- Stan_SalesTax_Forecast_2024[,1]
+Forecast2024 <- as.data.frame(Forecast2024)
+Forecast2024 <- Forecast2024 * (4.25/3.85)
+
+Actual2024 <- FinalData[(nrow(FinalData) - 11): nrow(FinalData), "Lead_STF_Real"]
+
+AE <- abs(Forecast2024 - Actual2024)
+MAE <- mean(AE[1,])
+print(round(MAE, 6))
+# Error is lower without Lead_STF
+
+# 2025
+# Time series object
+FinalData <- read_excel("Data/FinalData.xlsx")
+FinalData <- FinalData |> dplyr::select(-1) |> dplyr::select(-Licenses, -Lead_STF)
 FinalData$Lead_STF_Real <- FinalData$Lead_STF_Real * (3.85/4.25)
 FinalData$EDUHS[1:12] <- FinalData$EDUHS[1:12] - 5
 FinalData$SG[1:12] <- FinalData$SG[1:12] + 5
 
-# Time series object
-ts.object <- ts(FinalData$Lead_STF_Real, start = c(2012, 1), frequency = 12)
-# Bayesian model
-sf1 <- stan_sarima(ts = ts.object, order = c(0,0,0), seasonal = c(1,1,1),
-                   prior_mu0 = student(mu = 0, sd = 1,df = 155))
+# ldf_all <- log(FinalData)
+#
+# ts.object <- ts(ldf_all$Lead_STF_Real, start = c(2012, 1), frequency = 12)
+# # Bayesian model
+# sf_2025 <- stan_sarima(ts = ts.object, order = c(0,0,0), seasonal = c(1,1,1),
+#                    prior_mu0 = student(mu = 0, sd = 1, df = nrow(FinalData)-1))
+# # Bayesian forecasting
+# Stan_SalesTax_Forecast <- forecast(sf_2025, h = 11)
+# Stan_SalesTax_Forecast_2025 <- as.data.frame(Stan_SalesTax_Forecast)
+# Stan_SalesTax_Forecast_2025$`Point Forecast` <- exp(Stan_SalesTax_Forecast_2025$`Point Forecast`)
+# Stan_SalesTax_Forecast_2025$`Point Estimate` <- Stan_SalesTax_Forecast_2025$`Point Estimate` * (4.25/3.85)
+# View(Stan_SalesTax_Forecast_2025)
+#
+# forecast_plot1 <- autoplot(Stan_SalesTax_Forecast)
+# forecast_plot1
+
+
+ldf_all <- log(FinalData)
+
+ts.object_2025 <- ts(ldf_all$Lead_STF_Real, start = c(2012, 1), frequency = 12)
+
+sf_2025 <- stan_sarima(ts = ts.object_2025, order = c(0,0,0), seasonal = c(1,1,1),
+                       prior_mu0 = student(mu = 0, sd = 1, df = nrow(ldf_all)-1))
 # Bayesian forecasting
-Stan_SalesTax_Forecast <- forecast(sf1, h = 12)
+Stan_SalesTax_Forecast <- forecast(sf_2025, h = 11)
 Stan_SalesTax_Forecast_2025 <- as.data.frame(Stan_SalesTax_Forecast)
-View(Stan_SalesTax_Forecast_2025)
+Stan_SalesTax_Forecast_2025$`Point Forecast` <- exp(Stan_SalesTax_Forecast_2025$`Point Forecast`)
 
-forecast_plot1 <- autoplot(Stan_SalesTax_Forecast)
-forecast_plot1
+Forecast2025 <- Stan_SalesTax_Forecast_2025[,1]
+Forecast2025 <- as.data.frame(Forecast2025)
+Forecast2025 <- Forecast2025 * (4.25/3.85)
+
+forecast_plot <- autoplot(Stan_SalesTax_Forecast)
+forecast_plot
 
 
-# #SARIMA object
-# sf2 <- auto.sarima(ts = FinalData$Lead_STF_Real, seasonal = TRUE, iter = 5000, chains = 4)
-# # regular SARIMA forecasting model
-# Sarima_SalesTax_Forecast <- forecast(object = sf2, h = 12)
-# Sarima_SalesTax_Forecast_2025 <- as.data.frame(Sarima_SalesTax_Forecast)
-# View(Sarima_SalesTax_Forecast_2025)
-# forecast_plot2 <- autoplot(Sarima_SalesTax_Forecast)
-# forecast_plot2
 
-# Using stan_sarima yields more realistic results than auto.sarima.
-# However, the minimum sales tax value seems improbable, and might be influenced by the drastic dip from 2020.
-# Stan_sarima maintains the upward trend of sales tax, while auto.sarima is more stationary in nature.
-# auto.sarima may do better including priors. Stan_sarima does worse with uninformative priors.
-# Waiting for the ARMA unit in ECON436
+
+
+ts.object_2025_noLog <- ts(FinalData$Lead_STF_Real, start = c(2012, 1), frequency = 12)
+
+sf_2025noLog <- stan_sarima(ts = ts.object_2025noLog, order = c(0,0,0), seasonal = c(1,1,1),
+                       prior_mu0 = student(mu = 0, sd = 1, df = nrow(FinalData)-1))
+# Bayesian forecasting
+Stan_SalesTax_Forecast <- forecast(sf_2025noLog, h = 11)
+Stan_SalesTax_Forecast_2025 <- as.data.frame(Stan_SalesTax_Forecast)
+#Stan_SalesTax_Forecast_2025$`Point Forecast` <- exp(Stan_SalesTax_Forecast_2025$`Point Forecast`)
+
+Forecast2025 <- Stan_SalesTax_Forecast_2025[,1]
+Forecast2025 <- as.data.frame(Forecast2025)
+Forecast2025 <- Forecast2025 * (4.25/3.85)
+
+forecast_plot <- autoplot(Stan_SalesTax_Forecast)
+forecast_plot
+
